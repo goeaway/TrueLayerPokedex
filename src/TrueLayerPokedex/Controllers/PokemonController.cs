219892b0b7184982b41c32ef84b70001db57bd00
Joe Thompson <joseph.thompson>
2022-02-18 08:13:11 +0000

    Initial commit: Added project files, test files and deploy dockerfile

---
+++ src/TrueLayerPokedex/Controllers/PokemonController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TrueLayerPokedex.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PokemonController : ControllerBase 
+    {
+        public IActionResult Test()
+        {
+            return Ok("success");
+        }
+    }
+}
